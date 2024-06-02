12d37a47f73c2d46d73f2ca2e7b64c491a3d9236
Indira Rully Pricilia <92698567+Indirarully@users.noreply.github.com>
2024-06-02 10:23:32 +0700

    Update sql.h

--- app/sql.h
+++ app/sql.h
@@ -1,16 +1,9 @@
 //
-// Created by Jaelani on 25/05/2024.
+// Created by Indiraa on 02/06/2024.
 //
 
 #ifndef DATABASESQLITE_SQL_H
 #define DATABASESQLITE_SQL_H
-
-
-
 class sql {
-
 };
-
-
-
 #endif //DATABASESQLITE_SQL_H
